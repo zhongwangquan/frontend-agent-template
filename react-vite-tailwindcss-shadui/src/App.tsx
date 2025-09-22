ebb075ceaaf7158c3fcfe132216d3abeb43b23f2
1813114234@qq.com <1813114234@qq.com>
2025-09-22 18:25:36 +0800

    feat: 默认模板

--- react-vite-tailwindcss-shadui/src/App.tsx
+++ react-vite-tailwindcss-shadui/src/App.tsx
@@ -1,12 +1,35 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 function App() {
   return (
     <>
       <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button variant="default" className="!text-primary-foreground">
-          Click me
-        </Button>
+        <Button variant="default">Click me</Button>
+        <Input />
+        <Checkbox />
+        <Dialog>
+          <DialogTrigger>Open</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </>
   );
